4d591aeca733a36a1c052c9befb284e02981ac15
hrisabhy <hrisabhyadav31@gmail.com>
2023-12-15 22:18:16 +0530

    Popular Products added

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,7 +1,32 @@
+import PopularProductCard from "../components/PopularProductCard";
+import { products } from "../constants";
+
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
-  )
-}
+    <section
+      id="products"
+      className="max-container
+    max-sm:mt-12"
+    >
+      <div
+        className="flex
+      flex-col justify-start gap-5"
+      >
+        <h2 className="font-bold font-palanquin text-4xl">Our <span className="text-coral-red">Popular</span> Products</h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+          Experience top notch quality and style with our sought-after
+          selections. Discover a world of comfort, design and values
+        </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 sm:gap-6 gap-14">
+        {
+          products.map((product) => (
+            <PopularProductCard key={product.name} {...product} />
+          ))
+        }
+      </div>
+    </section>
+  );
+};
 
-export default PopularProducts
+export default PopularProducts;
