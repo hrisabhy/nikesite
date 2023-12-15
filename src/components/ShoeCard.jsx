42f9c20c15f15b67c526880badbf36073ec95290
hrisabhy <hrisabhyadav31@gmail.com>
2023-12-15 17:53:19 +0530

    Hero added

---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,10 @@
+const ShoeCard = ({imgUrl, changeBigShoeImage, biShoeImg    }) => {
+  return (
+    <div className={`border-2 
+    rounded-xl`}>
+        ShoeCard
+    </div>
+  )
+}
+
+export default ShoeCard
